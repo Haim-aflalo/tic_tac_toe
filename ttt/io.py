19a0a1184a97220c960eae6a15a34a46d58e01d2
Haim Aflalo <kodkodhaim@gmail.com>
2025-10-30 15:53:57 +0200

    function choose_position

--- ttt/io.py
+++ ttt/io.py
@@ -4,3 +4,27 @@ def choose_player():
         if player not in ["c","m"]:
             continue
         return player
+
+def choose_position(size = 3):
+    while True:
+        line = input("choose the line")
+        if not line.isdigit():
+            print("only numbers...")
+            continue
+        else:
+            lin = int(line)
+        if lin > size:
+            print("out of the board")
+            continue
+        col = input("choose the column")
+        if not col.isdigit():
+            print("only numbers...")
+            continue
+        else:
+            coll = int(col)
+        if coll > size:
+            print("out of the board")
+            continue
+
+        return lin,coll
+print(choose_position())
